d00811e1b16e7df252e807e674ba4c8bd17e9f4f
Musta <musta@XGP-M.local>
2017-08-01 11:04:49 -0700

    Add MinMax.c

---
+++ MinMax.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+// sizeof
+int main(){
+    printf("The max value you can store in (int) varible is %d \n",INT_MIN);;
+    printf("The max value you can store in (float) varible is %d \n",100);
+    
+    printf("The max value you can store in (char) varible is %d \n",100);
+
+    printf("The max value you can store in (int) varible is %d \n",100);
+    
+    printf("Hellow, world\n");
+
+    return 0;
+}
